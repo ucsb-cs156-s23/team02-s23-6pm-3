88d35b2f8d477c50c9ebe7355d729667b518e787
MrJacobEisner <mrjeisner@gmail.com>
2023-05-09 15:12:35 -0700

    WIP add article files

---
+++ src/main/java/edu/ucsb/cs156/example/repositories/Article.java
@@ -0,0 +1,13 @@
+package edu.ucsb.cs156.example.repositories;
+
+import edu.ucsb.cs156.example.entities.Article;
+
+import org.springframework.beans.propertyeditors.StringArrayPropertyEditor;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface ArticleRepository extends CrudRepository<Article, String> {
+ 
+}
